77d5efd559979e0e0649db48f23e5644ecdc81f0
Daniel Barreto <daniel.barreto.n@gmail.com>
2017-10-16 21:22:17 +0200

    Add README file

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil .
+      ((cider-boot-parameters . "dev")
+       (cider-cljs-boot-repl . "(do (require 'weasel.repl.websocket) (cemerick.piggieback/cljs-repl (weasel.repl.websocket/repl-env :ip \"127.0.0.1\" :port 9001)))"))))
